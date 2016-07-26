d7706c30078b5e15e9294f86f7c4e7370d0cec90
Mihail Selezniov <mihailselezniov@yandex.ru>
2016-07-26 15:39:16 +0300

    Update bot

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 import telebot, requests
 
+
 token = '265784305:AAEyapE7qBFPvObRUIvhWyIMSoIpbv6JvRE'
 bot = telebot.TeleBot(token)
 
+
 @bot.message_handler(content_types=["text"])
 def main(message):
-    if "p24" == message.text:
+    # Exchange rates with Privat24
+    if "p24" == message.text.lower():
         p24_url = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3"
         r = requests.get(p24_url)
         response = r.json()
@@ -14,9 +17,11 @@ def main(message):
         for row in response:
             msg += "{0}: {2} {1} / {3} {1}\n".format(row["ccy"], row["base_ccy"], row["buy"], row["sale"])
         bot.send_message(message.chat.id, msg)
+    # Calculator
     elif not sum(map(lambda x: int(x.isalpha()), message.text)):
         msg = str(eval(message.text))
         bot.send_message(message.chat.id, msg)
+    # Weather
     else:
         r = requests.post('https://genesis-mihailselezniov.c9users.io/weather', json={"city_name":message.text})
         response = r.json()
@@ -30,6 +35,26 @@ def main(message):
             msg = u"{}\n{}°\nВлажность: {}%\nДавление: {} мм\nСкорость ветра: {} м/сек".format(
                 city_name, temperature, dampness, pressure, wind_speed)
             bot.send_message(message.chat.id, msg)
+        # Translation suggestions
+        else:
+            msg = message.text
+            ru, en = 0, 0
+            for i in msg:
+                if u'а'<=i<=u'я' or u'А'<=i<=u'Я':
+                    ru += 1
+                elif u'a'<=i<=u'z' or u'A'<=i<=u'Z':
+                    en += 1
+            if ru > en:
+                lang="ru-en"
+            else:
+                lang="en-ru"
+            key = "trnsl.1.1.20160725T230300Z.5c6a542dc27e3a7d.3baed373aed1a3a990ad18f8e652adea67d14f72"
+            url_translate_yandex = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+            r = requests.post(url_translate_yandex, data = {'key':key, 'text':msg, 'lang':lang})
+            r_json = r.json()
+            if r_json["code"] == 200:
+                translate_msg = r_json["text"][0]
+                bot.send_message(message.chat.id, translate_msg)
     
     
 if __name__ == '__main__':
